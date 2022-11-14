ffbdd369bd2b08897487d39cabb077a2d2c61db5
unknown <sifatniloy18@gmail.com>
2022-11-14 14:47:01 -0600

    set admin

--- index.js
+++ index.js
@@ -45,7 +45,20 @@ async function run() {
       const users= await userCollection.find().toArray();
       res.send(users);
     })
-    
+
+    app.put('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      
+      const filter = { email: email };
+   
+      const updateDoc = {
+        $set:{role: 'admin'},
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      
+      res.send( result);
+    })
+
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
