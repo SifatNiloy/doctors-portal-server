426f12843d546e6e46cee498639ee79a6ee2177b
unknown <sifatniloy18@gmail.com>
2022-11-22 21:39:00 -0600

    payment confirmation email from backend done

--- index.js
+++ index.js
@@ -70,6 +70,37 @@ function sendAppointmentEmail(booking) {
     }
   });
 }
+function sendPaymentConfirmationEmail(booking) {
+  const { patient, patientName, treatment, date, slot } = booking;
+
+  var email = {
+
+    from: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `We have received your payment for ${treatment} is on ${date} at ${slot} is confirmed`,
+    text: `Your payment for this Appointment:  ${treatment} is on ${date} at ${slot} is confirmed`,
+    html: `
+    <div>
+    <h2>Hello ${patientName}</h2>
+    <h3> Thank you for your payment. </h3>
+    <p>Looking forward to see you on on ${date} at ${slot} </p>
+    <h3>Our address</h3>
+    <p>26/2 Banani, Dhaka</p>
+    <p>Bangladesh</p>
+    <a href="https://www.programming-hero.com/">unsubscribe</a>
+    </div>
+    `
+  };
+
+  emailClient.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      console.log('Message sent : ', info)
+    }
+  });
+}
 
 async function run() {
   try {
